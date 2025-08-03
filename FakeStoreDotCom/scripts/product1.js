8c6d2edc57f2e0a6e72211d50fb992dc80008da8
Raj Vardhan <errvmaurya563@gmail.com>
2025-08-03 08:33:58 +0530

    Refactor product display in product1.js and update HTML structure in product1.html to include category and rating information.

--- FakeStoreDotCom/scripts/product1.js
+++ FakeStoreDotCom/scripts/product1.js
@@ -1,82 +1,55 @@
 async function getProducts() {
-  const response = await fetch("https://fakestoreapi.in/api/products");
+  const response = await fetch("https://fakestoreapi.com/products");
   const data = await response.json();
 
-  // console.log(data);
+  const products = document.getElementById("ProductList");
 
-  const products = data.products;
+  data.forEach((element) => {
+    const productRow = document.createElement("div");
+    productRow.classList.add(
+      "row",
+      "border",
+      "border-dark",
+      "rounded",
+      "shadow",
+      "p-3"
+    );
 
-  //   for (var i = 0; i < products.length; i++) {
-  //     console.log(products[i]);
-  //   }
-
-  let productList = document.getElementById("ProductList");
-
-  products.forEach((element) => {
-    // console.log(element);
-
-    let d = document.createElement("div");
-    d.classList.add("row", "border", "border-dark", "rounded", "shadow", "p-3");
-
-    if (element.discount) {
-      d.innerHTML = `
-                <div class="col-3 border border-danger">
+    productRow.innerHTML = `
+          <div class="col-3 border border-danger">
+            <div class="d-flex justify-content-center align-items-center">
             <img
               src=${element.image}
-              alt="ProductImage"
-              width="100%"
-              class="object-fit-cover"
+              alt=""
+              width="200px"
+              height="200px"
+              class="object-fit-contain mt-5 "
             />
-          </div>
-          <div class="col-9 border border-success p-4">
-            <h2>${element.title}</h2>
-            <span class="fs-6"><b>Brand:</b> ${element.brand}</span>
-            <div class="d-grid mt-4">
-              <span class="text-danger">Limited Time Deal... Hurry Up!!!</span>
-              <span class="fs-5">
-                <b>Price:</b> <span class="text-success fw-bold">₹${
-                  element.price * 85 - (element.price * element.discount) / 100
-                }</span>
-              </span>
-              <span class="text-primary-emphasis">
-                <b>MRP:</b> <del>₹${element.price * 85}</del>
-              </span>
             </div>
+          </div>
+          <div class="col-9 border border-success p-4 d-flex flex-column gap-2">
+            <h2>${element.title.length >50 ? element.title.slice(0,50) + " ..." : element.title}</h2>
+            <span class="fs-6"><b>Category:</b> ${element.category}</span>
+            <span class="">
+              <b>Rating:</b> ${element.rating.rate}/5 (${
+      element.rating.count
+    } reviews)
+            </span>
+            <span class="fs-5">
+              <b>Price:</b> <span class="text-success fw-bold">₹${(
+                element.price * 85
+              ).toFixed(2)}</span>
+            </span>
+
             <div class="mt-4 d-flex gap-3">
               <button class="btn btn-warning">Buy Now</button>
               <button class="btn btn-outline-warning">Add to Cart</button>
             </div>
-    `;
-    } else {
-      d.innerHTML = `
-                <div class="col-3 border border-danger">
-            <img
-              src=${element.image}
-              alt="ProductImage"
-              width="100%"
-              class="object-fit-cover"
-            />
           </div>
-          <div class="col-9 border border-success p-4">
-            <h2>${element.title}</h2>
-            <span class="fs-6"><b>Brand:</b> ${element.brand}</span>
-            
-              <span class="fs-5">
-                <b>Price:</b> <span class="text-success fw-bold">₹${
-                  element.price * 85
-                }</span>
-              </span>
-              
-            <div class="mt-4 d-flex gap-3">
-              <button class="btn btn-warning">Buy Now</button>
-              <button class="btn btn-outline-warning">Add to Cart</button>
-            </div>
     `;
-    }
 
-    productList.appendChild(d);
+    products.appendChild(productRow);
   });
 }
 
-
-getProducts();  
+getProducts();
